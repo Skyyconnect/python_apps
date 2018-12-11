f9f78436b300028f221edb2f23341228c5131ef4
Skyyler Siejko <skyyconnected@gmail.com>
2018-12-11 01:05:12 -0800

    Update cards.py
    updated syntax

--- cards.py
+++ cards.py
@@ -1,29 +1,62 @@
-from random import randint,shuffle
+from random import randint
+
+suits = ["Hearts", "Diamonds", "Spade", "Clubs"]
+values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "k", "A"]
 
 
-card = ["[2]","[3]","[4]","[5]","[6]","[7]","[8]","[9]","[10]","[J]","[Q]","[K]","[A]",]
-suit = ["Hearts", "Clubs", "Spades", "Diamonds"]
-deck = []
-class Cards:
-  def __init__(self, deck):
-    self.card = card
-    self.suits = suit
-    self.deck = deck 
+class Card:
+  def __init__(self, suit, value):
+    self.suit = suit
+    self.value = value
   
   
-  def makeDeck(self):
-    self.deck = []
-    for i in xrange(len(card)):
-      for j in range(len(suit)):
-        self.card = card[i]+ suit[j]
-        self.deck.append(self.card)
-    print len(self.deck)
-    return self.deck
+  
+  
+class Deck:
+  def __init__(self):
+    self.cards = []
+    self.hands = {}
     
-  def shuffler(self):
-    return shuffle(self.deck)
+  def createDeck(self):
+    for i in range(len(values)):
+      for j in range(len(suits)):
+        self.cards.append(Card(suits[j], values[i]))
+    return self.cards
     
-cardDeck = Cards(deck)
-cardDeck.makeDeck()
-cardDeck.shuffler()
-print cardDeck.deck
+  def shuffle(self, newDeck, deckLength):
+    if len(newDeck) < deckLength:
+      card = self.cards[randint(0, len(self.cards)-1)]
+      newDeck.append(card)
+      self.cards.remove(card)
+      return self.shuffle(newDeck, deckLength)
+    else:
+      self.cards = newDeck
+      return self.cards
+
+  def get(self, attr):
+    values = []
+    for each in self.cards:
+      if attr == "values":
+        values.append(each.value)
+      else:
+        values.append(each.suit)
+    return values
+    
+  def draw(self):
+    for each in self.cards:
+      print each.value + " of " + each.suit
+
+ 
+deck = Deck()
+deck.createDeck()
+deck.shuffle([], 52)
+
+
+hand = deck.cards[:5]
+for each in hand:
+  print each.value, each.suit
+
+
+   
+    
+  
